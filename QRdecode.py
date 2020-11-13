bc18374449376f6e34347b224e93ec7d605c8541
南叔 <62488207+a327777892@users.noreply.github.com>
2020-11-14 02:25:47 +0800

    Update QRdecode.py
    二维码解析小工具,手机无法运行

--- QRdecode.py
+++ QRdecode.py
@@ -1,38 +1,38 @@
-＃-*-编码：UTF-8-*-
-＃xizhi撰写
+# -*- coding: UTF-8 -*-
+# Written by xizhi
 
-导入 操作系统
-导入 时间
-尝试：
-  来自 pyzbar。pyzbar 导入 解码
-除了 ImportError：
-  打印（“ pyzbar模块没有安装好哦\ n 5秒后自动退出”）
-  时间。睡觉（5）
-  os。_退出（0）
-尝试：
-  从 PIL 导入 图片
-除了 ImportError：
-  打印（“ pyzbar模块的依赖库pillow没有安装好哦\ n 5秒后自动退出”）
-  时间。睡觉（5）
-  os。_退出（0）
+import os
+import time
+try:
+  from pyzbar.pyzbar import decode
+except ImportError:
+  print("pyzbar 模块没有安装好哦\n5秒后自动退出")
+  time.sleep(5)
+  os._exit(0)
+try:
+  from PIL import Image
+except ImportError:
+  print("pyzbar 模块的依赖库 pillow 没有安装好哦\n5秒后自动退出")
+  time.sleep(5)
+  os._exit(0)
 
-打印（“ \ n请复制粘贴二维码的图片路径（含后缀）或拖拽二维码的图片到该窗口” \
-      “ \ n如果不想每次都输入路径请将二维码的图片移动到该目录下，然后名字强制改为q.png，名字需要完全一样，直接按确定即可” \
-      “ \ n如果一个图片包含多个二维码的会批量解析完”）
-qrdecodeimg  =  input（“支持常见的图片后缀（gif，png，jpg等）：”）。replace（“ \” “，”“）。replace（” \' “，”“）
-如果 qrdecodeimg  ==  “”：
-  qrdecodeimg  =  “ q.png”
-尝试：
-  图片。打开（qrdecodeimg）。转换（“ RGBA”）。保存（qrdecodeimg）
-  qrdecodel  = []
-  qrdecode  = 解码（图片。打开（qrdecodeimg））
-  对于 内容 中 qrdecode：
-    qrdecodel。append（内容。数据。解码（“ utf-8”））
-  qrdecode  =  “ \ n ”。加入（qrdecodel）
-  打印（“ \ n解码内容为：\ n \ n％s \ n \ n将以上内容分别复制到需要的地方即可，程序30秒后自动退出” ％（qrdecode））
-  时间。睡觉（30）
-  os。_退出（0）
-除了 FileNotFoundError：
-  打印（“ \ n该目录下没有％s文件哦\ n 5秒后自动退出” ％（qrdecodeimg））
-  时间。睡觉（5）
-  os。_退出（0）
+print("\n请复制粘贴二维码的图片路径(含后缀)或拖拽二维码的图片到该窗口"\
+      "\n如果不想每次都输入路径请将二维码的图片移动到该目录下,然后名字强制改为 q.png ,名字需要完全一样,直接按确定即可"\
+      "\n如果一个图片包含多个二维码的会批量解析完")
+qrdecodeimg = input("支持常见的图片后缀(gif,png,jpg等):").replace("\"","").replace("\'","")
+if qrdecodeimg == "":
+  qrdecodeimg = "q.png"
+try:
+  Image.open(qrdecodeimg).convert("RGBA").save(qrdecodeimg)
+  qrdecodel = []
+  qrdecode = decode(Image.open(qrdecodeimg))
+  for content in qrdecode:
+    qrdecodel.append(content.data.decode("utf-8"))
+  qrdecode = "\n".join(qrdecodel)
+  print("\n解码内容为:\n\n%s\n\n将以上内容各自复制粘贴到需要的地方即可,程序30秒后自动退出"%(qrdecode))
+  time.sleep(30)
+  os._exit(0)
+except FileNotFoundError:
+  print("\n该目录下没有 %s 文件哦\n5秒后自动退出"%(qrdecodeimg))
+  time.sleep(5)
+  os._exit(0)
