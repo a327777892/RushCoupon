d209016feb69784c610750772bf2159297c68343
南叔 <62488207+a327777892@users.noreply.github.com>
2020-12-04 00:32:29 +0800

    Update AllinOne1py3.py

--- AllinOne1py3.py
+++ AllinOne1py3.py
@@ -33,27 +33,27 @@ class Unifri1():
     if re.findall(r"未登录",str(unifrigoodsq1)) != []:
       print("返回信息: "+unifrigoodsq1["msg"]+"\n联通登录状态失效了,请重新获取Cookie")
       AllinOneExit1()
-    unifriactL1 = unifrigoodsq1["resdata"]["activityList"]
+    unifritabL1 = unifrigoodsq1["resdata"]["tabList"]
     unifrigoodsnl1 = []
     unifrigoodsnl11 = []
     unifrigoodsidl1 = []
     unifripaypril1 = []
     unifrigoodsbtl1 = []
     a = 0
-    unifristate1 = {"10":"立即抢购","20":"去查看","30":"无法抢购","40":"已抢光","50":"未开始"}
-    for b in range(0,len(unifriactL1)):
-      for i,goods in enumerate(unifriactL1[b]["goodsList"],a+1):
-        unifrigoodsnl11.append(str(i)+" "+unifriactL1[b]["navClock"]+\
+    unifristate1 = {"00":"暂未开始","10":"立即抢购","20":"去查看","30":"无法抢购","40":"已抢光","50":"未开始"}
+    for b in range(0,len(unifritabL1)):
+      for i,goods in enumerate(unifritabL1[b]["goodsList"],a+1):
+        unifrigoodsnl11.append(str(i)+" "+unifritabL1[b]["timeNav"]+\
                                               unifristate1.get(goods["state"])+" "+goods["goodsName"])
         unifrigoodsnl1.append(goods["goodsName"])
         nowdate1 = time.strftime("%Y-%m-%d",time.localtime(int(time.time())))
-        actLtimes1 = int(time.mktime(time.strptime(nowdate1+" "+unifriactL1[b]["navClock"]+":00",
+        actLtimes1 = int(time.mktime(time.strptime(nowdate1+" "+unifritabL1[b]["timeNav"]+":00",
                                    "%Y-%m-%d %H:%M:%S"))*1000)
         unifrigoodsbtl1.append(actLtimes1)
       a = i
-      for goods in unifriactL1[b]["goodsList"]:
+      for goods in unifritabL1[b]["goodsList"]:
         unifrigoodsidl1.append(goods["goodsId"])
-      for goods in unifriactL1[b]["goodsList"]:
+      for goods in unifritabL1[b]["goodsList"]:
         unifripaypril1.append(goods["price"]+"0")
     if re.findall(r"fourNineGoodsList",str(unifrigoodsq1),flags=re.I) != []:
       for i,goods in enumerate(unifrigoodsq1["resdata"]["fourNineGoodsList"],i+1):
@@ -86,7 +86,7 @@ class Unifri1():
 
   def UnifriLocalGoods1(self):
     global unifrigoodsn1,unifrigoodsid1,unifripaypri1,unifrigoodsbt1
-    unifrigoodsid1 = linecache.getline(r"unifri1cfg.set",24).strip()
+    unifrigoodsid1 = linecache.getline(r"unifri1cfg.set",26).strip()
     if unifrigoodsid1 == "8a29ac8a72a48dbe0172bb4885430d81":
       unifrigoodsn1 = "美团5元"
       unifripaypri1 = "2.00"
@@ -114,9 +114,9 @@ class Unifri1():
   def UnifriGetOrders1(self):
     try:
       global unifriorders1
-      unifriorderj1 = requests.post("https://m.client.10010.com/welfare-mall-front/mobile/api/bj2402/v1",
+      unifriorderj1 = requests.post("http://m.client.10010.com/welfare-mall-front/mobile/api/bj2402/v1",
                                                    headers=unifriheaders1,params=unifridata1,
-                                                   timeout=float(linecache.getline(r"unifri1cfg.set",32).strip())).json()
+                                                   timeout=float(linecache.getline(r"unifri1cfg.set",34).strip())).json()
       unifriorders1 = unifriorderj1["msg"]
     except KeyboardInterrupt:
       print("用户中断操作")
@@ -137,7 +137,7 @@ class Unifri1():
 
   def UnifriOrdering1(self):
     try:
-      unifriftime1 = linecache.getline(r"unifri1cfg.set",29).strip()
+      unifriftime1 = linecache.getline(r"unifri1cfg.set",31).strip()
       unifriftimes1 = 1
       if int(linecache.getline(r"unifri1cfg.set",15).strip()) == 1:
         unifrirt1 = []
@@ -147,10 +147,12 @@ class Unifri1():
             unifrirt1.append(timef)
         self.UnifriGettime1()
         for timef in unifrirt1:
+          unifriet1 = linecache.getline(r"unifri1cfg.set",19).strip()
           unifriwm1 = linecache.getline(r"unifri1cfg.set",17).strip()
           unifriwt1 = (datetime.datetime.strptime(timef,"%H:%M:%S.%f")+datetime.timedelta(minutes=-int(unifriwm1))).strftime("%H:%M:%S.%f")[:-3]
           unifriwt11 = (datetime.datetime.strptime(timef,"%H:%M:%S.%f")+datetime.timedelta(minutes=-1)).strftime("%H:%M:%S.%f")[:-3]
           if unifritime1 >= unifriwt1 and unifritime1 < timef:
+            timef = (datetime.datetime.strptime(timef,"%H:%M:%S.%f")+datetime.timedelta(milliseconds=-int(unifriet1))).strftime("%H:%M:%S.%f")[:-3]
             print("请勿关闭,程序将在 %s 开抢"%(timef))
             while unifritime1 > unifriwt1 and unifritime1 < timef:
               if unifritime1 < unifriwt11:
@@ -160,7 +162,7 @@ class Unifri1():
                 print("当前联通的时间是: %s ,每隔0.01秒刷新时间"%(unifritime1),end="\r")
                 time.sleep(0.01)
               self.UnifriGettime1()
-        time.sleep(float(linecache.getline(r"unifri1cfg.set",19).strip()))
+        time.sleep(float(linecache.getline(r"unifri1cfg.set",21).strip()))
       self.UnifriGetOrders1()
       while re.findall(r"下单成功",str(unifriorders1)) == []:
         print(("返回信息: "+unifriorders1).ljust(50),end="\r")
@@ -168,22 +170,27 @@ class Unifri1():
           print("返回信息: "+unifriorders1)
           print("该账号已有订单,不能再次购买\n")
           AllinOneExit1()
-        elif re.findall(r"下单成功|存在未支付的订单",str(unifriorders1)) != []:
+        elif re.findall(r"下单成功",str(unifriorders1)) != []:
           break
         unifriftimes1 += 1
         if unifriftimes1 % 20 == 0:
           try:
             unifriwporderj1 = requests.post("https://m.client.10010.com/welfare-mall-front/mobile/show/bj3034/v1",
                                                    headers=unifriheaders1,params="reqsn=&reqtime=0&cliver=&reqdata=%7B%7D",
-                                                   timeout=float(linecache.getline(r"unifri1cfg.set",32).strip())).json()
+                                                   timeout=float(linecache.getline(r"unifri1cfg.set",34).strip())).json()
             unifriwporders1 = unifriwporderj1["resdata"]["orderCount"]["wait_pay_order"]
             if int(unifriwporders1) > 0:
               print("该账号有未支付订单,请尽快支付,逾期将失效哦")
-              if int(linecache.getline(r"unifri1cfg.set",35).strip()) == 1:
-                times = time.strftime("%H{}%M{}%S{}").format("时","分","秒")   #加入时间,避免造成重复消息导致Server酱无法推送
-                requests.get("https://sc.ftqq.com/%s.send?text=%s Unifri1的账号有未支付订单,请尽快支付,逾期将失效哦"\
-                            %(linecache.getline(r"unifri1cfg.set",37).strip(),times))
-              AllinOneExit1()
+              for files in os.walk(os.getcwd()):
+                if re.findall(r".*提醒过未支付订单了\.reminded",str(files),flags=re.I) != []:
+                  pass
+                else:
+                  if int(linecache.getline(r"unifri1cfg.set",37).strip()) == 1:
+                    times = time.strftime("%H{}%M{}%S{}").format("时","分","秒")   #加入时间,避免造成重复消息导致Server酱无法推送
+                    requests.get("https://sc.ftqq.com/%s.send?text=%s Unifri1的账号有未支付订单,请尽快支付,逾期将失效哦"\
+                            %(linecache.getline(r"unifri1cfg.set",39).strip(),times))
+                    with open(time.strftime("%H{}%M{}%S{}").format("时","分","秒")+" 提醒过未支付订单了.reminded","w") as ordered:
+                      pass
           except:pass
         print(("没有下单成功,将在%s秒后第%s次刷新"%(unifriftime1,unifriftimes1)).ljust(50),end="\r")
         time.sleep(float(unifriftime1))
@@ -192,23 +199,23 @@ class Unifri1():
       with open("Unifri1的商品 "+unifrigoodsn1+" "+\
                       time.strftime("%H{}%M{}%S{}").format("时","分","秒")+"下单成功.ordered","w") as ordered:
         print("已记录Unifri1的商品:%s 下单成功时间"%(unifrigoodsn1))
-      if int(linecache.getline(r"unifri1cfg.set",35).strip()) == 1:
+      if int(linecache.getline(r"unifri1cfg.set",37).strip()) == 1:
         times = time.strftime("%H{}%M{}%S{}").format("时","分","秒")   #加入时间,避免造成重复消息导致Server酱无法推送
         requests.get("https://sc.ftqq.com/%s.send?text=%s %s 下单成功,请尽快在30分钟内支付,逾期将失效哦"\
-                            %(linecache.getline(r"unifri1cfg.set",37).strip(),times,unifrigoodsn1))
+                            %(linecache.getline(r"unifri1cfg.set",39).strip(),times,unifrigoodsn1))
       AllinOneExit1()
     except (requests.exceptions.Timeout,requests.exceptions.ConnectionError,ValueError):
       self.UnifriOrdering1()
   
   def UnifriMain1(self):
     global unifriheaders1,unifridata1
-    rechangeno1 = linecache.getline(r"unifri1cfg.set",26).strip()
+    rechangeno1 = linecache.getline(r"unifri1cfg.set",28).strip()
     print("\n正在运行联通超级星期五\n当前配置的对应手机号为: %s\n"%(rechangeno1))
     unifriheaders1 = {"User-Agent":"Mozilla/5.0 (Linux;Android 10;GM1910) AppleWebKit/\
                                 537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36; \
-                                unicom{version:android@8.0000}",
-                                "Cookie":"%s"%(linecache.getline(r"unifri1cfg.set",40).strip())}
-    if int(linecache.getline(r"unifri1cfg.set",22).strip()) == 1:
+                                unicom{version:android@8.0002}",
+                                "Cookie":"%s"%(linecache.getline(r"unifri1cfg.set",42).strip())}
+    if int(linecache.getline(r"unifri1cfg.set",24).strip()) == 1:
       self.UnifriLocalGoods1()
       unifrigoodsq1 = requests.get("https://m.client.10010.com/welfare-mall-front-activity/mobile/activity/get619Activity/v1?whetherFriday=YES",
                                                      headers=unifriheaders1,timeout=3).json()
@@ -218,7 +225,7 @@ class Unifri1():
     else:
       self.UnifriNetGoods1()
     for files in os.walk(os.getcwd()):
-      if re.findall("Unifri1的商品 %s.*\.ordered"%(unifrigoodsn1),str(files),flags=re.I) != []:
+      if re.findall(r"Unifri1的商品 %s.*\.ordered"%(unifrigoodsn1),str(files),flags=re.I) != []:
         print("该Unifri1的商品: %s 已下单成功了,如果需要再次下单,请先删除目录下对应的.ordered文件"%(unifrigoodsn1))
         AllinOneExit1()
     unifridata1 = 'reqsn=&reqtime=&cliver=&reqdata={"goodsId":"%s","payWay":"01",'\
@@ -586,7 +593,7 @@ def AllinOneMain1():
 
 try:
   unifrilines1 = len(open(r"unifri1cfg.set",errors="ignore",encoding="UTF-8").readlines())
-  if unifrilines1 != 40:
+  if unifrilines1 != 42:
     print("出错了, unifri1cfg.set 的行数不对哦")
     AllinOneExit1()
 except FileNotFoundError:
